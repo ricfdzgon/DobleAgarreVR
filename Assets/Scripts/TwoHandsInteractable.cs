4229884e70433a4378279428684592bd0b2b959d
Ricardo Fernández <ricfdzgon@gmail.com>
2022-05-19 18:13:49 +0200

    Primer agarre

---
+++ Assets/Scripts/TwoHandsInteractable.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class TwoHandsInteractable : XRGrabInteractable
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+   
+}
