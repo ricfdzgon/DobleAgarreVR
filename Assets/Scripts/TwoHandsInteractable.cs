81af54df746b740eec02788b827656235cbf58fd
Ricardo Fernández <ricfdzgon@gmail.com>
2022-05-19 19:17:35 +0200

    "Agarre" con dos manos y rotación atrás

--- Assets/Scripts/TwoHandsInteractable.cs
+++ Assets/Scripts/TwoHandsInteractable.cs
@@ -8,21 +8,58 @@ public class TwoHandsInteractable : XRGrabInteractable
     //Referencia al interactable para el agarre de la segunda mano
     public XRSimpleInteractable secondGrabPoint;
 
+    private XRBaseInteractor secondInteractor;
+
+    private Quaternion initialRotation;
+
     void Start()
     {
         secondGrabPoint.onSelectEntered.AddListener(OnSecondInteractorGrab);
         secondGrabPoint.onSelectExited.AddListener(OnSecondInteractorRelease);
+
+    }
+
+    public override void ProcessInteractable(XRInteractionUpdateOrder.UpdatePhase updatePhase)
+    {
+        base.ProcessInteractable(updatePhase);
+
+        if (secondInteractor != null)
+        {
+            selectingInteractor.attachTransform.rotation = GetRotation();
+        }
+    }
+
+    private Quaternion GetRotation()
+    {
+        Quaternion targetRotation;
+
+        targetRotation = Quaternion.LookRotation(secondInteractor.transform.position - selectingInteractor.transform.position, secondInteractor.transform.up);
+
+        return targetRotation;
     }
 
     public void OnSecondInteractorGrab(XRBaseInteractor interactor)
     {
         Debug.Log("Agarro con la segunda mano");
+        secondInteractor = interactor;
     }
 
 
     public void OnSecondInteractorRelease(XRBaseInteractor interactor)
     {
         Debug.Log("Suelto la segunda mano");
+        secondInteractor = null;
+    }
 
+    protected override void OnSelectEntered(XRBaseInteractor interactor)
+    {
+        base.OnSelectEntered(interactor);
+        initialRotation = interactor.transform.rotation;
+    }
+
+    protected override void OnSelectExited(XRBaseInteractor interactor)
+    {
+        base.OnSelectExited(interactor);
+        interactor.transform.rotation = initialRotation;
     }
 }
