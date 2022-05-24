99a2ee58958238522946ac86ccfbf5219e535419
Ricardo Fernández <ricfdzgon@gmail.com>
2022-05-24 18:34:13 +0200

    Disparo bala

---
+++ Assets/Scripts/RifleShot.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RifleShot : MonoBehaviour
+{
+    public GameObject balaPrefab;
+    float shotForce = 100;
+    public Transform shotPoint;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void Shot()
+    {
+        GameObject bala = Instantiate(balaPrefab, shotPoint.position, shotPoint.rotation);
+        bala.GetComponent<Rigidbody>().AddForce(bala.transform.forward * shotForce, ForceMode.Impulse);
+    }
+}
